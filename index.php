5e77ed21a6644bd44cf745907c13680af429b0d0
Gonzalo Civita <gonzalocivita@gmail.com>
2024-10-23 18:46:35 -0300

    Corrección de los styles del nav-bar

--- index.php
+++ index.php
@@ -10,42 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="styles.css" />
-    <style>
-        /* Personalización del botón hamburguesa */
-        .navbar-toggler {
-            border-color: transparent;
-            /* Quitar el borde del botón */
-        }
-
-        .navbar-toggler-icon {
-            background-image: none;
-            /* Eliminar el icono por defecto */
-        }
-
-        /* Diseño personalizado para el botón */
-        .navbar-toggler-icon::before {
-            content: '\2630';
-            /* Símbolo de hamburguesa */
-            font-size: 1.5rem;
-            /* Tamaño del símbolo */
-            color: #fff;
-            /* Color del símbolo */
-        }
-
-        /* Fondo personalizado del botón cuando está colapsado */
-        .navbar-toggler {
-            background-color: #007bff;
-            /* Color de fondo personalizado */
-            border-radius: 5px;
-            /* Bordes redondeados */
-        }
-
-        /* Cambiar el color del botón al pasar el mouse */
-        .navbar-toggler:hover {
-            background-color: #0056b3;
-            /* Fondo más oscuro al hacer hover */
-        }
-    </style>
 </head>
 
 <body>
